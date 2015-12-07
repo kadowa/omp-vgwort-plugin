86afde0e5bed6ebe1f6acdafccd4ea565f4cf097
Katharina Wäschle <waeschle@ub.uni-heidelberg.de>
2015-12-07 10:33:04 +0100

    Basic plugin structure

---
+++ VGWortPlugin.inc.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * @file plugins/generic/vgWort/VGWortPlugin.inc.php
+ *
+ * Copyright (c) 2013-2015 Simon Fraser University Library
+ * Copyright (c) 2003-2015 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class VGWortPlugin
+ * @ingroup plugins_generic_vgwort
+ *
+ * @brief VG Wort plugin class
+ */
+
+import('lib.pkp.classes.plugins.GenericPlugin');
+
+class VGWortPlugin extends GenericPlugin {
+
+	/**
+	 * Called as a plugin is registered to the registry
+	 * @param $category String Name of category plugin was registered to
+	 * @return boolean True iff plugin initialized successfully; if false,
+	 * 	the plugin will not be registered.
+	 */
+	function register($category, $path) {
+		$success = parent::register($category, $path);
+		if ($success && $this->getEnabled()) {
+
+		}
+		return $success;
+	}
+
+	function getDisplayName() {
+		return __('plugins.generic.vgWort.displayName');
+	}
+
+	function getDescription() {
+		return __('plugins.generic.vgWort.description');
+	}
+	
+}
+?>
---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @defgroup plugins_generic_vgwort
+ */
+
+/**
+ * @file plugins/generic/vgWort/index.php
+ *
+ * Copyright (c) 2013-2015 Simon Fraser University Library
+ * Copyright (c) 2003-2015 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @ingroup plugins_generic_vgwort
+ * @brief Wrapper for VG Wort plugin.
+ *
+ */
+require_once('VGWortPlugin.inc.php');
+
+return new VGWortPlugin();
+
+?>
